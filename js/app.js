b15d0cef9eed63caa2d7df4424acc6323591dc9b
Nate <josiahuks@gmail.com>
2022-07-03 14:59:42 -0400

    Modified index, and I added javascript files, css files, and modified the node server file.

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// "use strict";
+
+// request = new XMLHttpRequest();
+
+// request.addEventListener("load", function(){
+//     console.log("Reqeust made");
+// });
+
+// request.open("GET", "localhost:3000/notes/note1.txt");
+// request.send();
